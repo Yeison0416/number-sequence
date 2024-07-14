c3e0982a3e685a03aa880e86234feae5ae472df3
Sancy061 <Yeison.S.Sanchez.-ND@disney.com>
2024-07-13 22:05:35 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import './index.scss';
 
 function component() {
     const element = document.createElement('p');
-    element.textContent = 'hello webpac dkashbdkjashdkjhsajkdhjkas';
+    element.textContent = 'hello webpack';
 
-    const p2 = document.createElement('p');
-    const numbers1 = [1, 2, 3, 4, 5, 6];
-    const numbers2 = [7, 8, 9, 10];
-    const numbers3 = [...numbers1, ...numbers2];
-    p2.textContent = numbers3.join(' ');
-    document.body.appendChild(p2);
     return element;
 }
 
