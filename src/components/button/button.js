9fa9bd881a8c4648b985a90c10c7afe5cd398c35
Sancy061 <Yeison.S.Sanchez.-ND@disney.com>
2024-09-18 10:00:01 -0500

    handling button states

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -7,25 +7,57 @@ import styles from './button.scss';
 const FALLBACK_BUTTON_DATA = [
     {
         buttonLabel: ['start', 'stop'],
-        currentState: false,
+        labelState: 0,
+        state: false,
     },
     {
         buttonLabel: ['reset'],
-        currentState: false,
+        labelState: 0,
+        state: false,
     },
 ];
 
 const button = (buttonData = FALLBACK_BUTTON_DATA) => {
+    const handleButtonsClickEvent = (startButton, resetButton) => {
+        startButton.startButtonNode.addEventListener('click', () => {
+            startButton.labelState = (startButton.labelState + 1) % startButton.buttonLabel.length;
+            startButton.startButtonNode.innerText = startButton.buttonLabel[startButton.labelState];
+            resetButton.resetButtonNode.disabled = false;
+        });
+
+        resetButton.resetButtonNode.addEventListener('click', () => {
+            resetButton.resetButtonNode.disabled = true;
+            startButton.labelState = 0;
+            startButton.startButtonNode.innerText = startButton.buttonLabel[startButton.labelState];
+        });
+    };
+
     const state = {
         type: 'button',
         buildButtons() {
+            const startButtonObject = {};
+            const resetButtonObject = {};
+
             const buttonsContainer = createTags('div', 'buttons-container');
+            const startButton = createTags('button', 'start-button');
+            const resetButton = createTags('button', 'reset-button');
 
             buttonData.forEach((button) => {
-                const buttonElement = createTags('button', 'button');
-                button?.currentState ? (buttonElement.innerText = button.buttonLabel[1]) : (buttonElement.innerText = button.buttonLabel[0]);
-                buttonsContainer.appendChild(buttonElement);
+                if (button.buttonLabel.length - 1 > 0) {
+                    startButton.innerText = button.buttonLabel[button?.labelState ?? 0];
+                    startButton.disabled = button?.state ?? false;
+                    Object.assign(startButtonObject, { startButtonNode: startButton }, button);
+                } else {
+                    resetButton.innerText = button.buttonLabel[button.labelState ?? 0];
+                    resetButton.disabled = button?.state ?? true;
+                    Object.assign(resetButtonObject, { resetButtonNode: resetButton }, button);
+                }
             });
+
+            buttonsContainer.appendChild(startButton);
+            buttonsContainer.appendChild(resetButton);
+
+            handleButtonsClickEvent(startButtonObject, resetButtonObject);
 
             return buttonsContainer;
         },
